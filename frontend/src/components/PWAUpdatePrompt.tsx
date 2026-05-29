d521e3adce95a14d54d5b89170541dd022749ec8
LePingouin <oligoudreault@gmail.com>
2026-05-29 19:49:32 -0400

    New try

--- frontend/src/components/PWAUpdatePrompt.tsx
+++ frontend/src/components/PWAUpdatePrompt.tsx
@@ -8,7 +8,7 @@ export function PWAUpdatePrompt() {
 
   useEffect(() => {
     if (!('serviceWorker' in navigator)) return;
-console.log('[PWA] PWAUpdatePrompt mounted v4');
+console.log('[PWA] PWAUpdatePrompt mounted v5');
 
     const cleanups: (() => void)[] = [];
 
