30b60cf7d31717e75dcfa089d6f61645147b282b
LePingouin <oligoudreault@gmail.com>
2026-09-03 08:37:46 -0400

    Added important

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.eaq9mub509k"
+    "revision": "0.ttuncpn4964"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
